3baf65ac2fdc76766e24c9fea0753e7c0b4caba4
june.park <laurenceyoon@gmail.com>
2021-09-26 02:32:21 +0900

    update extracting features

--- hw1/feature_extraction.py
+++ hw1/feature_extraction.py
@@ -29,39 +29,61 @@ def extract_mfcc(dataset='train'):
         file_path = data_path + file_name
         y, sr = librosa.load(file_path, sr=SAMPLE_RATE)
 
-        ##### Method 1
-        mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=MFCC_DIM)
-        
-        ##### Method 2 
-        """
         # STFT
-        S = librosa.core.stft(y, n_fft=1024, hop_length=512, win_length=1024)
-
+        S = librosa.core.stft(y=y, n_fft=N_FFT, hop_length=HOP_LENGTH, win_length=WIN_LENGTH)
         # power spectrum
-        D = np.abs(S)**2
+        D = np.abs(S) ** 2
 
-        # mel spectrogram (512 --> 40)
-        mel_basis = librosa.filters.mel(sr, 1024, n_mels=40)
+        # mel spectrogram
+        mel_basis = librosa.filters.mel(sr=sr, n_fft=N_FFT, n_mels=MEL_BINS)
         mel_S = np.dot(mel_basis, D)
 
-        #log compression
+        # log compression
         log_mel_S = librosa.power_to_db(mel_S)
 
-        # mfcc (DCT)
-        mfcc = librosa.feature.mfcc(S=log_mel_S, n_mfcc=13)
-        mfcc = mfcc.astype(np.float32)    # to save the memory (64 to 32 bits)
-        """
+        # MFCC (DCT)
+        mfcc = librosa.feature.mfcc(sr=sr, S=log_mel_S, n_mfcc=MFCC_DIM)
+        mfcc = mfcc.astype(np.float32)  # to save the memory (64 to 32 bits)
 
-        # save mfcc as a file
-        file_name = file_name.replace('.wav','.npy')
+        # Add delta & second delta of mfcc
+        delta_mfcc = librosa.feature.delta(mfcc)
+        delta_delta_mfcc = librosa.feature.delta(mfcc, order=2)
+
+        # rms & zero crossing rate feature
+        f_rms = librosa.feature.rms(y=y, hop_length=HOP_LENGTH, frame_length=WIN_LENGTH)
+        f_zero_crossing_rate = librosa.feature.zero_crossing_rate(y=y, frame_length=WIN_LENGTH, hop_length=HOP_LENGTH)
+
+        # spectral statistics features
+        centroid = librosa.feature.spectral_centroid(S=mel_S, n_fft=N_FFT, win_length=WIN_LENGTH, hop_length=HOP_LENGTH)
+        # flatness = librosa.feature.spectral_flatness(S=mel_S, n_fft=N_FFT, hop_length=HOP_LENGTH, win_length=WIN_LENGTH)
+        # ^-- commented out because all datasets are not white noise.
+        bandwidth = librosa.feature.spectral_bandwidth(S=mel_S, n_fft=N_FFT, hop_length=HOP_LENGTH, win_length=WIN_LENGTH)
+        contrast = librosa.feature.spectral_contrast(S=mel_S, n_fft=N_FFT, hop_length=HOP_LENGTH, win_length=WIN_LENGTH)
+
+        # concatenate all features
+        features = np.concatenate(
+            [
+                mfcc,
+                delta_mfcc,
+                delta_delta_mfcc,
+                f_rms,
+                f_zero_crossing_rate,
+                centroid,
+                bandwidth,
+                contrast,
+            ],
+            axis=0,
+        )
+
+        # save mfcc features as a file
+        file_name = file_name.replace('.wav', '.npy')
         save_file = mfcc_path + file_name
 
         if not os.path.exists(os.path.dirname(save_file)):
             os.makedirs(os.path.dirname(save_file))
-        np.save(save_file, mfcc)
+        np.save(save_file, features)
 
 
 if __name__ == '__main__':
     extract_mfcc(dataset='train')                 
     extract_mfcc(dataset='valid')                                  
-
