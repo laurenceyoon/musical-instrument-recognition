7658870c5d31c1f7ae6296aaaf1a48c323f7d48b
june.park <laurenceyoon@gmail.com>
2021-09-26 02:45:14 +0900

    apply five different classifiers on training test

--- hw1/train_test.py
+++ hw1/train_test.py
@@ -9,37 +9,153 @@ import sys
 import os
 import numpy as np
 import librosa
+from sklearn.linear_model import SGDClassifier
+from sklearn.svm import NuSVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from texttable import Texttable
+
 from feature_summary import *
 
-from sklearn.linear_model import SGDClassifier
+
+def extract_accuracy(classifier, train_X, train_Y, valid_X, valid_Y):
+    classifier.fit(train_X, train_Y)
+
+    # validation
+    valid_Y_hat = classifier.predict(valid_X)
+    accuracy = np.sum((valid_Y_hat == valid_Y)) / 300.0 * 100.0
+
+    return classifier, accuracy
 
 
-def train_model(train_X, train_Y, valid_X, valid_Y, hyper_param1):
-
-    # Choose a classifier (here, linear SVM)
-    clf = SGDClassifier(
+def _train_by_SGD_classifier(train_X, train_Y, valid_X, valid_Y, alpha):
+    classifier = SGDClassifier(
         verbose=0,
         loss="hinge",
-        alpha=hyper_param1,
+        alpha=alpha,
         max_iter=1000,
         penalty="l2",
         random_state=0,
     )
+    return extract_accuracy(classifier, train_X, train_Y, valid_X, valid_Y)
 
-    # train
-    clf.fit(train_X, train_Y)
 
-    # validation
-    valid_Y_hat = clf.predict(valid_X)
+def _train_by_KNN_classifier(train_X, train_Y, valid_X, valid_Y, algorithm, weight):
+    classifier = KNeighborsClassifier(
+        n_neighbors=10, algorithm=algorithm, weights=weight
+    )
+    return extract_accuracy(classifier, train_X, train_Y, valid_X, valid_Y)
 
+
+def _train_by_NuSVC_classifier(train_X, train_Y, valid_X, valid_Y, nu, kernel):
+    classifier = NuSVC(nu=nu, kernel=kernel)
+    return extract_accuracy(classifier, train_X, train_Y, valid_X, valid_Y)
+
+
+def _train_by_MLP_classifier(
+    train_X, train_Y, valid_X, valid_Y, solver, learning_rate, alpha
+):
+    classifier = MLPClassifier(
+        activation="relu",
+        solver=solver,
+        max_iter=5000,
+        learning_rate=learning_rate,
+        alpha=alpha,
+    )
+    return extract_accuracy(classifier, train_X, train_Y, valid_X, valid_Y)
+
+
+def _train_by_GMM_classifier(train_X, train_Y, valid_X, valid_Y):
+    classifier = GaussianProcessClassifier()
+    return extract_accuracy(classifier, train_X, train_Y, valid_X, valid_Y)
+
+
+def train_model(train_X, train_Y, valid_X, valid_Y, model: str, table: Texttable):
+    classifier, accuracy = None, None
+    classifiers = []
+    valid_acc = []
+
+    # Choose a classifier (here, linear SVM)
+    if model == "SGD":  # 96%
+        alphas = [0.0001, 0.001, 0.01, 0.1, 1, 10]
+        for alpha in alphas:
+            classifier, accuracy = _train_by_SGD_classifier(
+                train_X, train_Y, valid_X, valid_Y, alpha
+            )
+            classifiers.append(classifier)
+            valid_acc.append(accuracy)
+            table.add_row([str(classifier), f"alpha={alpha}", "X", accuracy])
+    elif model == "K-NN":  # 89.667% (weight: distance)
+        algorithms = ["auto", "ball_tree", "kd_tree", "brute"]
+        weights = ["uniform", "distance"]
+        for algorithm in algorithms:
+            for weight in weights:
+                classifier, accuracy = _train_by_KNN_classifier(
+                    train_X, train_Y, valid_X, valid_Y, algorithm, weight
+                )
+                classifiers.append(classifier)
+                valid_acc.append(accuracy)
+                table.add_row(
+                    [
+                        str(classifier),
+                        f"algorithm={algorithm}, weight={weight}",
+                        "X",
+                        accuracy,
+                    ]
+                )
+    elif model == "NuSVC":  # 97.333% (nu: 0.1, kernel: rbf)
+        nus = [0.1, 0.3, 0.5, 0.9]
+        kernels = ["linear", "poly", "rbf", "sigmoid"]
+        for nu in nus:
+            for kernel in kernels:
+                classifier, accuracy = _train_by_NuSVC_classifier(
+                    train_X, train_Y, valid_X, valid_Y, nu, kernel
+                )
+                classifiers.append(classifier)
+                valid_acc.append(accuracy)
+                table.add_row(
+                    [str(classifier), f"nu={nu}, kernel={kernel}", "X", accuracy]
+                )
+    elif (
+        model == "MLP"
+    ):  # [MAX] 97.667% (activation: relu, alpha: 0.1, solver: adam, learning_rate: constant)
+        alphas = [0.0001, 0.001, 0.01, 0.1, 1, 10]
+        activations = ["identity", "logistic", "tanh", "relu"]
+        solvers = ["lbfgs", "sgd", "adam"]
+        learning_rates = ["constant", "invscaling", "adaptive"]
+        for solver in solvers:
+            for learning_rate in learning_rates:
+                for alpha in alphas:
+                    classifier, accuracy = _train_by_MLP_classifier(
+                        train_X, train_Y, valid_X, valid_Y, solver, learning_rate, alpha
+                    )
+                    classifiers.append(classifier)
+                    valid_acc.append(accuracy)
+                    table.add_row(
+                        [
+                            str(classifier),
+                            f"alpha={alpha}, solver={solver}, learning_rate={learning_rate}",
+                            "X",
+                            accuracy,
+                        ]
+                    )
+    elif model == "GMM":  # 94.333%
+        classifier, accuracy = _train_by_GMM_classifier(
+            train_X, train_Y, valid_X, valid_Y
+        )
+        classifiers.append(classifier)
+        valid_acc.append(accuracy)
+        table.add_row([str(classifier), "", "X", accuracy])
+
+    final_model = classifiers[np.argmax(valid_acc)]
+    valid_Y_hat = final_model.predict(valid_X)
     accuracy = np.sum((valid_Y_hat == valid_Y)) / 300.0 * 100.0
-    print("validation accuracy = " + str(accuracy) + " %")
-
-    return clf, accuracy
+    table.add_row([str(final_model), "", "O", accuracy])
+    return final_model, accuracy, table
 
 
 if __name__ == "__main__":
-
     # load data
     train_X = mean_mfcc("train")
     valid_X = mean_mfcc("valid")
@@ -49,7 +165,7 @@ if __name__ == "__main__":
     train_Y = np.repeat(cls, 110)
     valid_Y = np.repeat(cls, 30)
 
-    # feature normalizaiton
+    # feature normalization
     train_X = train_X.T
     train_X_mean = np.mean(train_X, axis=0)
     train_X = train_X - train_X_mean
@@ -60,15 +176,20 @@ if __name__ == "__main__":
     valid_X = valid_X - train_X_mean
     valid_X = valid_X / (train_X_std + 1e-5)
 
-    # training model
-    alphas = [0.0001, 0.001, 0.01, 0.1, 1, 10]
-
     model = []
     valid_acc = []
-    for a in alphas:
-        clf, acc = train_model(train_X, train_Y, valid_X, valid_Y, a)
-        model.append(clf)
-        valid_acc.append(acc)
+    algorithms = ["SGD", "K-NN", "NuSVC", "MLP", "GMM"]
+    for algorithm in algorithms:
+        table = Texttable(max_width=300)
+        table.header(["classifier", "hyper param", "is final?", "accuracy"])
+        final_model, accuracy, table = train_model(
+            train_X, train_Y, valid_X, valid_Y, algorithm, table
+        )
+        model.append(final_model)
+        valid_acc.append(accuracy)
+        table.set_deco(Texttable.HEADER)
+        print(f"[{algorithm}]")
+        print(table.draw() + "\n")
 
     # choose the model that achieve the best validation accuracy
     final_model = model[np.argmax(valid_acc)]
@@ -77,4 +198,4 @@ if __name__ == "__main__":
     valid_Y_hat = final_model.predict(valid_X)
 
     accuracy = np.sum((valid_Y_hat == valid_Y)) / 300.0 * 100.0
-    print("final validation accuracy = " + str(accuracy) + " %")
+    print(f"final validation accuracy is {accuracy} with {final_model}")
